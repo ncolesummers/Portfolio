e8320e5300296fde043cb30fbfe5a257ccfcc4fc
ncolesummers <nsummers72@gmail.com>
2021-07-19 19:25:06 +0000

    continuing nextjs tutorials

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,16 +1,8 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/const '
+import { getSortedPostsData } from '../lib/posts'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
 
 export default function Home({ allPostsData }) {
   return (
@@ -43,3 +35,11 @@ export default function Home({ allPostsData }) {
   )
 }
 
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
