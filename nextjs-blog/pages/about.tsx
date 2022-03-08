b81cb3060541a454c37dd6e0d70730bf3da09c28
Nathan Summers <nsummers72@gmail.com>
2022-03-07 23:26:45 -0800

    Update about.tsx

--- nextjs-blog/pages/about.tsx
+++ nextjs-blog/pages/about.tsx
@@ -16,7 +16,7 @@ export default function About() {
                 />
             </div>
             <section className={utilStyles.headingMd}>
-                <p>👋Hi!  I'm Cole Summers, a software developer and WebAssembly afficionado looking for remote or hybrid work in an inclusive environment.</p>
+                <p>👋Hi!  I'm Cole Summers, a software developer and WebAssembly aficionado looking for remote or hybrid work in an inclusive environment.</p>
                 <p>When I'm not behind a computer you can find me with my nose in a book or dancing the night away.</p>
                 <p>If you need someone who can ship reliable software, slide into my DMs using one of the links below, or <a href = "mailto:nsummers72@gmail.com?subject = Development Request">email</a> me. </p>
             </section>
